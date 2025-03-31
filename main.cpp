2f64f393e5dbf027511b824a42c2fe4781290123
junios-study <31557850+junios-study@users.noreply.github.com>
2025-03-31 16:39:10 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//Number
+//[][][][] -> 100
+
+int main()
+{
+	int Number1 = 5;
+	int Number2 = 4;
+
+	//Çüº¯È¯, Casting
+	float A = 5.5f;
+	int B = static_cast<int>(A);
+
+	return 0;
+}
